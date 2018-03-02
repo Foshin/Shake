2692cd7974255fffe858c1b3cb79652f62f1b0d9
Foshin <shopoledi@yandex.ru>
2018-03-02 08:22:39 +0300

    Shake

--- Shake/Shake/Program.cs
+++ Shake/Shake/Program.cs
@@ -10,7 +10,12 @@ namespace Shake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
 
             Console.ReadLine();
         }
