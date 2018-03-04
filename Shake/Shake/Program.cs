87f61dc9663ade4dfe0294b7fe47c6a86d2f5441
Foshin <shopoledi@yandex.ru>
2018-03-04 16:53:25 +0300

    Списки

--- Shake/Shake/Program.cs
+++ Shake/Shake/Program.cs
@@ -10,44 +10,20 @@ namespace Shake
     {
         static void Main(string[] args)
         {
-            int x = 1;
-            Func1(x);
-            Console.WriteLine("Func1.x = " + x);
-            x = 1;
-            Func2(x);
-            Console.WriteLine("Func2.x = " + x);
-            x = 1;
-            Func3(x);
-            Console.WriteLine("Func3.x = " + x);
+            Point t1 = new Point(0, 3, '@');
+            Point t2 = new Point(1, 5, '#');
+            Point t3 = new Point(2, 2, '$');
 
-            Point p1 = new Point(1, 3, '*');
-            Move(p1, 10, 10);
-            Console.WriteLine("Call Move. p1.x = " + p1.x + ", p1.y = " + p1.y);
+            List<Point> LP = new List<Point>();
+            LP.Add(t1);
+            LP.Add(t2);
+            LP.Add(t3);
 
-            Point p2 = new Point(4, 5, '#');
-            p1 = p2;
-            p2.x = 8;
-            p2.y = 8;
-            Console.WriteLine("p1 = p2. p1.x = " + p1.x + "p1.y = " + p1.y + ", p2.x = " + p2.x + ", p2.y = " + p2.y);
+            foreach (Point i in LP) i.Draw();
+
 
             Console.ReadLine();
         }
-        public static void Func1(int value)
-        {
 
-        }
-        public static void Func2(int value)
-        {
-            value++;
-        }
-        public static void Func3(int x)
-        {
-            x++;
-        }
-        public static void Move(Point p, int dx, int dy)
-        {
-            p.x += dx;
-            p.y += dy;
-        }
     }
 }
