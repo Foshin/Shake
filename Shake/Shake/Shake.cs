51b1676f366f978ee72b514444afcb1238551b67
Foshin <shopoledi@yandex.ru>
2018-03-07 11:36:46 +0300

    Змейка полная

---
+++ Shake/Shake/Shake.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shake
+{
+    class Shake:Figur
+    {
+        public Shake(Point End,int Rzm, Direct Direct)
+        {
+            pList = new List<Point>();
+            for (int i=0;i<Rzm;i++)
+            {
+                Point p = new Point(End);
+                p.Move(i, Direct);
+                pList.Add(p);
+            }
+        }
+    }
+}
